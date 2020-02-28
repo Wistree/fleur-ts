6a0a0f50ae5c315bb1a0d74e8448033fd38c3586
Sam Bui <buivietdung1995@gmail.com>
2020-02-28 20:33:02 +1100

    style currency

--- src/components/Currency.component.tsx
+++ src/components/Currency.component.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react"
+import styled from 'styled-components';
+
+export const DropBtn = styled.button`
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+`
+
+export const DropDown = styled.div`
+  position: relative;
+  display: inline-block;
+`
 
 const Currency = () => (
-  <div> Currency </div>
-);
+  <div className = "dropdown">
+    <button className="dropbtn"> Dropdown </button>
+    <div className="dropdown-content">
+      <a href="#"> USA </a>
+      <a href="#"> AUS </a>
+    </div>
+  </div>
+)
 
-export default Currency;
+export default Currency
