678d48a556d07b3db24006cf8e3edd855fd1de3a
Sahanah <sahanah.ganesh@gmail.com>
2020-02-28 18:05:41 +1100

    adapted prettier linter babel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Test } from './components/Test'
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { Test } from "./components/Test"
 
 function App() {
   return (
     <div>
-      <Test logo={logo}
+      <Test
+        logo={logo}
         text="Edit src/App.tsx and save to reload."
         text2="Learn React"
-        link="https://reactjs.org" />
+        link="https://reactjs.org"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../App.css'
+import React from "react"
+import "../App.css"
 
 interface IProps {
   logo: string
@@ -11,18 +11,11 @@ interface IProps {
 export const Test: React.FC<IProps> = ({ logo, text, text2, link }) => (
   <div className="App">
     <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        {text}
-      </p>
-      <a
-        className="App-link"
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          {text2}
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{text}</p>
+      <a className="App-link" href={link} target="_blank" rel="noopener noreferrer">
+        {text2}
+      </a>
     </header>
   </div>
 )
