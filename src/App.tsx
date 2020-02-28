e275d767ce61551810b2fcb57072faee1f57cb2b
Sahanah <sahanah.ganesh@gmail.com>
2020-02-28 19:25:28 +1100

    adds time assets

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import { Test } from "./components/Test"
+import styled from "styled-components"
+
+export const AppBox = styled.div`
+  text-align: center;
+`
 
 function App() {
   return (
-    <div>
+    <AppBox>
       <Test
         logo={logo}
         text="Edit src/App.tsx and save to reload."
         text2="Learn React"
         link="https://reactjs.org"
       />
-    </div>
+    </AppBox>
   )
 }
 
