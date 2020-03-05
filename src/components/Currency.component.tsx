2b90d3b0e334424e8f3c8c77ec1b44491e42b937
Sam Bui <buivietdung1995@gmail.com>
2020-03-05 19:59:22 +1100

    currency changing

--- src/components/Currency.component.tsx
+++ src/components/Currency.component.tsx
@@ -13,16 +13,29 @@ export const DropBtn = styled.button`
 export const DropDown = styled.div`
   position: relative;
   display: inline-block;
+  ${DropDownContent}:hover & {
+    display: block;
+  }
+`
+
+export const DropDownContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  padding: 12px 16px;
+  z-index: 1;
 `
 
 const Currency = () => (
-  <div className="dropdown">
-    <button className="dropbtn"> Dropdown </button>
-    <div className="dropdown-content">
+  <DropDown>
+    <DropBtn> Dropdown </DropBtn>
+    <DropDownContent>
       <a href="#"> USA </a>
       <a href="#"> AUS </a>
-    </div>
-  </div>
+    </DropDownContent>
+  </DropDown>
 )
 
 export default Currency
